0897e012148798c792317826784bfa70bb9614f7
Joe <spizjo01@gmail.com>
2022-09-22 10:14:58 -0600

    commit tree 81d9010297256e2470810e9c2264c2dfb7d136bc

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,16 @@
 class Company < ActiveRecord::Base
+has_many :freebies
+has_many :devs, through: :freebies
+    def give_freebie dev, item_name, value
+        Freebie.create(item_name: item_name, value: value, company_id: self.id, dev_id: dev.id)
+    end
+
+    def self.oldest_company
+        Company.order(founding_year: :asc).limit(1)
+    end
+
 
 end
+
+# Company.oldest_company
+# returns the Company instance with the earliest founding year
